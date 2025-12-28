2b354c4d8d167b3ae75dfab88824851c40353784
ImSpiDy <SpiDy2713@gmail.com>
2025-12-28 22:50:35 +0100

    Revert "lavender: Import liboemcrypto blobs from lavender V12.0.1.0.QFGMIXM"
    - L1 later
    
    This reverts commit 91b82ea1cbe8d803b61c697084b24100354eddc4.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -972,7 +972,6 @@ PRODUCT_PACKAGES += \
     libgf_ca \
     libgf_hal \
     libgoodixhwfingerprint \
-    liboemcrypto \
     libvendor.goodix.hardware.interfaces.biometrics.fingerprint@2.1 \
     vendor.qti.hardware.fingerprint@1.0 \
     dirac_resource \
